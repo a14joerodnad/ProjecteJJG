ee35cc04d50d81fab85f50bef6e7724889aadddb
a17jorcermar <usuario@debian-9.2.1-gnome-amd64>
2020-11-02 14:01:59 +0100

    montamos header

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Cantina Pedralbes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/cantina.css">
+
+</head>
+
+<header>
+    <ul>
+        <li class="nav-bar-title"><a class="active" href="index.php">Inici</a></li>
+        <li class="nav-bar-icon"><a href="admin.php"><i class="fas fa-user-shield"></i></a></li>
+        <li class="nav-bar-icon"><a href="index.php"><i class="fa fa-home"></i></a></li>
+    </ul>
+</header>
+</html>
