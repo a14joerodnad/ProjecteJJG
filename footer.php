28ebc267711942ed2babc3c8315fa64baf70c4de
a17jorcermar <usuario@debian-9.2.1-gnome-amd64>
2020-11-02 11:25:19 +0100

    añado footer

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
             <p> Contacta amb nosaltres:</p>
         </div>
         <div>
-            <p> 934778677/657665448     cantina@inspedralbes.cat </p>
+            <p> 934778677/657665648     cantina@inspedralbes.cat </p>
         </div>
     </div>
     
