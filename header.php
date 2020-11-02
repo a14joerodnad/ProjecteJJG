cf581c71a9ce9c1b319096575a03af0afe23008c
a17gerchifer <a17gerchifer@inspedralbes.cat>
2020-11-02 16:11:52 +0100

    css

--- header.php
+++ header.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>CANTINA PEDRALBES</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="css/cantina.css">
-
-</head>
-
 <header>
     <ul>
         <li class="nav-bar-title"><a class="active" href="index.php">Inici</a></li>
@@ -17,4 +5,3 @@
         <li class="nav-bar-title"><a class="active" href="admin.php">Admin</a></li>
     </ul>
 </header>
-</html>
