6c33c5bbb2b24b0f764586209b20d3274716c78d
a14joerodnad <a14joerodnad@inspedralbes.cat>
2020-11-02 12:07:58 +0100

    he borrat una linia del footer

--- footer.php
+++ footer.php
@@ -2,17 +2,15 @@
 <html>
 
 <body>
-    
-    include 
     <footer>
-    <div class="piepagina">
-        <div>
-            <p> Contacta amb nosaltres:</p>
+        <div class="piepagina">
+            <div>
+                <p> Contacta amb nosaltres:</p>
+            </div>
+            <div>
+                <p> 934778677/657665648     cantina@inspedralbes.cat </p>
+            </div>
         </div>
-        <div>
-            <p> 934778677/657665648     cantina@inspedralbes.cat </p>
-        </div>
-    </div>
     
     
     </footer>
