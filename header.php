7e38c4f45e4f4d193b34e1a7f6691b808fa89c75
a14joerodnad <a14joerodnad@inspedralbes.cat>
2020-11-02 16:20:00 +0100

    css del header actualitzat

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
     <ul>
         <li class="nav-bar-title"><a class="active" href="index.php">Inici</a></li>
-        <li class="nav-bar-title"><a class="active" href="menu.php">Carta</a></li>
+        <li class="nav-bar-title"><a class="active" href="menu.php">Menu</a></li>
         <li class="nav-bar-title"><a class="active" href="admin.php">Admin</a></li>
     </ul>
 </header>
