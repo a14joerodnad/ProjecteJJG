ef61c52d22a9578b9549ac0643ccf8690c3e1aec
a17gerchifer <a17gerchifer@inspedralbes.cat>
2020-11-04 13:36:15 +0100

    progreso carrito

---
+++ carro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <title>Login</title>
+      <link rel="stylesheet" type="text/css" href="css/css.css">
+      <link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
+      <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+      <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+      <link href="css/styles.css" rel="stylesheet" />
+   </head>
+   <body>
+     <?php include 'header.php';?>
+
+     
+     <?php include 'carroform.php';?>
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+      <script src="js/scripts.js"></script>
+   </body>
+</html>
