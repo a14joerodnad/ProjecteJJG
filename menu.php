4f571c62a843138d93e0113d9c10e1e324e57557
a17gerchifer <a17gerchifer@inspedralbes.cat>
2020-11-06 09:59:30 +0100

    d

--- menu.php
+++ menu.php
@@ -10,7 +10,8 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="css/styles.css" rel="stylesheet" />
     <link href="css/css.css" rel="stylesheet" />
-    <script src="script.js" defer></script>
+    <script src="js/scripts.js" language="Javascript"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
 </head>
 <body>
     <?php include 'header.php';?>
@@ -142,6 +143,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-    <script src="js/scripts.js"></script>
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
